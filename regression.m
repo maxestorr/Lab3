7161c8ebaa2643b2c66e5dc62858f91800c93818
Max Storr <43936387+maxestorr@users.noreply.github.com>
2019-11-26 17:46:11 +0000

    Delete regression.m
    Invalidated by linearRegression.m in master branch

--- regression.m
+++
@@ -1,6 +0,0 @@
-clear all
-clc
-%% Import Data
-[classData, labels, regData, targets] = get_data();
-%% Linear Classification test
-classMdl = fitrsvm(classData, labels(:,1), 'KernelFunction', 'linear', 'BoxConstraint', 1);
