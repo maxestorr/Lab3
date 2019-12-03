14ce7c71bb9d0b1c76091f905d8fe781c1a52bb7
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-12-03 09:03:56 +0000

    Update linearClassification.m

--- linearClassification.m
+++ linearClassification.m
@@ -1,5 +1,4 @@
 function linClassMdl = linearClassification(data,labels,boxConstraint)
-    linClassMdl = fitcsvm(data((1:74610),:),labels((1:74610),1),...
-        'KernelFunction','linear', 'BoxConstraint',boxConstraint);
+    linClassMdl = fitcsvm,labels, 'KernelFunction','linear', 'BoxConstraint',boxConstraint);
 end
 
