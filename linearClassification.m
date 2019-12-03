8ae39ed686fc75919b1a1f2409ad1cbf0a592ce3
Max Storr <43936387+maxestorr@users.noreply.github.com>
2019-12-03 09:47:20 +0000

    Update linearClassification.m
    Remove unnecessary blank lines

--- linearClassification.m
+++ linearClassification.m
@@ -2,6 +2,3 @@ function linClassMdl = linearClassification(data,labels,boxConstraint)
     linClassMdl = fitcsvm(data,labels,'KernelFunction','linear',...
         'BoxConstraint',boxConstraint);
 end
-
-
-
