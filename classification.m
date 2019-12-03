fa611c244488e5700cb229569e9c8bad88de9dcc
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-12-03 09:28:49 +0000

    Delete classification.m

--- classification.m
+++
@@ -1,6 +0,0 @@
-clear all
-clc
-%% Import Data
-[classData, labels, regData, targets] = getData();
-%% Linear Classification test
-classMdl = fitcsvm(classData, labels(:,1), 'KernelFunction', 'linear', 'BoxConstraint', 1);
