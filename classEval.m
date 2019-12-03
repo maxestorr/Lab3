f89c8e69d7ef6e8e353a0c8c1c9d5dead8785eb8
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-12-03 09:25:52 +0000

    Update classEval.m

--- classEval.m
+++ classEval.m
@@ -1,4 +1,4 @@
 function accuracy = classEval[classMdl,testData,testLabels]
   fit = predict(classMdl,testData));
-  accuracy = 100*(1-sum((fit - testLabels).^2)/(length(testLabels));
+  accuracy = 100*(1-sum(fit - testLabels)/(length(testLabels));
 end
