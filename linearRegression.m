64713d2ef9a9767eee50082c1a883fa18b35dffc
Kan98 <kan.srimanchantha@gmail.com>
2019-12-03 10:06:09 +0000

    fix

--- linearRegression.m
+++ linearRegression.m
@@ -1,4 +1,4 @@
-function regMdl = linearRegression(data, targets, boxConstraint, epsilonValue)
-regMdl = fitrsvm(data, targets(:,1), 'KernelFunction', 'linear', 'BoxConstraint', boxConstraint, ...
+function regMdl = linearRegression(data, targets, boxConstraint, epsilonValue, labelColumn)
+regMdl = fitrsvm(data, targets(:, labelColumn), 'KernelFunction', 'linear', 'BoxConstraint', boxConstraint, ...
                  'Epsilon', epsilonValue);
 end
