42330e9de78d45c10c6120077972abb2b6d9d262
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-12-03 10:42:46 +0000

    Linear classification (#17)
    * Add files via upload
    
    * Update linearClassification.m
    
    * Create classExample.m
    
    * Update linearClassification.m
    
    * determine classification rate
    
    * Delete classExample.m
    
    * Update classEval.m
    
    * Update linearClassification.m
    
    * Update classEval.m
    
    * Update linearClassification.m
    
    * Update linearClassification.m
    
    Remove unnecessary blank lines
    
    * Update classEval.m
    
    * Remove unequal parenthesis
    
    * Fix unequal parenthesis
    
    * Update classEval.m
    
    * Fix imbalanced parenthesis
    
    * Fixed accuracy>100 using abs()
    
    * Camelcase functions

---
+++ linearClassification.m
@@ -0,0 +1,4 @@
+function linClassMdl = linearClassification(data,labels,boxConstraint)
+    linClassMdl = fitcsvm(data,labels,'KernelFunction','linear',...
+        'BoxConstraint',boxConstraint);
+end
