26e9cf3835835b50263e44e7adb961b9ddb94cd2
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-12-03 08:55:00 +0000

    Update linearClassification.m

--- linearClassification.m
+++ linearClassification.m
@@ -1,8 +1,5 @@
-clear all
-clc
-%% Import Data
-[classData, labels, regData, targets] = getData();
-%% Linear Classification test
-classMdl = fitcsvm(classData((1:74610),:),labels((1:74610),1), 'KernelFunction','linear', 'BoxConstraint',1);
-fit = predict(classMdl,classData((74611:82906),:));
-accuracy = 100*(1-sum((fit - labels((74611:82906),1)).^2)/(8295));
+function linClassMdl = linearClassification(data,labels,boxConstraint)
+    linClassMdl = fitcsvm(data((1:74610),:),labels((1:74610),1),...
+        'KernelFunction','linear', 'BoxConstraint',boxConstraint);
+end
+
