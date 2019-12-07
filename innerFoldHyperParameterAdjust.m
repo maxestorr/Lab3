3cbb23f8d7e3518142c9c4b476f0e69f037785e7
maxestorr <maxestorr@gmail.com>
2019-12-07 23:59:00 +0000

    Returns array of best parameters using new method

--- innerFoldHyperParameterAdjust.m
+++ innerFoldHyperParameterAdjust.m
@@ -1,10 +1,9 @@
 function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels, modelSelection)
     k_sliceNum = 10;
     [new_features, new_labels] = kFold(k_sliceNum, test_features, test_labels);
-    avgAcc = [];
-    accuracyArray = [];
+    paramEval = []
     
-    % k-fold
+    % k-fold (NEEDS REIMPLEMENTING SINCE I FUCKED IT)
     for i = 1 : k_sliceNum
         feature_test = new_features(:,:, i);
         label_test = new_labels(:, i);
@@ -33,14 +32,16 @@ function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels,
                     linearReg = linearRegression(feature_train, label_train, epsilon, constraint, 1);
                     predictions = predict(linearReg, feature_test);
                     %L2 error used here, gimme ideas bois
-                    accuracyArray = [accuracyArray, (1 / 2 * length(label_test)) * sumsqr(predictions - label_test)];
+                    acc = (1 / 2 * length(label_test)) * sumsqr(predictions - label_test);
+                    paramEval = [paramEval; constraint, epsilon, acc];
                 end
             end
         case 2
-            for boxConstraint = 10:100:10                
-                linearClass = linearClassification(feature_train, label_train, boxConstraint);
+            for constraint = 10:100:10                
+                linearClass = linearClassification(feature_train, label_train, constraint);
                 predictions = predict(linearClass, feature_test);
-                accuracyArray = [accuracyArray, sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum)];
+                acc = sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum);
+                paramEval = [paramEval; constraint, acc];
             end
         case 3
             for constraint = 10:100:10
@@ -48,7 +49,8 @@ function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels,
                     for polyOrder = 1:5:1
                         polyReg = polynomialRegression(feature_train, label_train, constraint, epsilon, 1, polyOrder);
                         predictions = predict(polyReg, feature_test);
-                        accuracyArray = [accuracyArray, (1 / 2 * length(label_test)) * sumsqr(predictions - label_test)];
+                        acc = (1 / 2 * length(label_test)) * sumsqr(predictions - label_test);
+                        paramEval = [paramEval; constraint, epsilon, polyOrder, acc];
                     end
                 end
             end
@@ -57,7 +59,8 @@ function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels,
                 for polyOrder = 1:5:1
                     polyClass = polynomialClassification(feature_train, label_train, constraint, polyOrder, 1);
                     predictions = predict(polyClass, feature_test);
-                    accuracyArray = [accuracyArray, sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum)];
+                    acc = sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum);
+                    paramEval = [paramEval; constraint, polyOrder, acc];
                 end
             end
         case 5
@@ -66,7 +69,8 @@ function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels,
                     for sigma = 1:10:1
                         rbfReg = rbfRegMdl(feature_train, label_train, constraint, epsilon, 1, sigma);
                         predictions = predict(rbfReg, feature_test);
-                        accuracyArray = [accuracyArray, (1 / 2 * length(label_test)) * sumsqr(predictions - label_test)];
+                        acc = (1 / 2 * length(label_test)) * sumsqr(predictions - label_test);
+                        paramEval = [paramEval; constraint, epsilon, sigma, acc];
                     end
                 end
             end
@@ -75,16 +79,17 @@ function [bestParam] = innerFoldHyperParameterAdjust(test_features, test_labels,
                 for sigma = 1:10:1
                     rbfClass = rbfClassMdl(feature_train, label_train, constraint, sigma, 1);
                     predictions = predict(rbfClass, feature_test);
-                    accuracyArray = [accuracyArray, sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum)];
+                    acc = sum(predictions == label_test) / (size(test_features, 1) / k_sliceNum);
+                    paramEval = [paramEval; constraint, sigma, acc];
                 end
             end
         otherwise
             warning('modelSelection param not properly assigned. Pls check.')
     end
-avgAcc = [avgAcc, mean(accuracyArray)];
-accuracyArray = [];
-best_index = find(avgAcc == max(avgAcc(:)));
-bestParam = params(best_index(1));
+
+% Find row of parameters for best accuracy
+[bestRows, ~] = find(paramEval(:, end) == max(paramEval(:, end)));
+bestParams = paramEval(bestRows(1), 1:end-1);  % Exclude accuracy value
 end
 
 
