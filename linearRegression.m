39a97f06607dee5bd9258c3724f04d6dcf610f2d
kan98 <34004612+kan98@users.noreply.github.com>
2019-12-08 20:23:39 +0000

    added display statements (#37)

--- linearRegression.m
+++ linearRegression.m
@@ -1,4 +1,5 @@
 function regMdl = linearRegression(data, targets, boxConstraint, labelColumn, epsilonValue)
+disp('Linear reg');
 regMdl = fitrsvm(data, targets(:, labelColumn), 'KernelFunction', 'linear', 'BoxConstraint', boxConstraint, ...
                  'Epsilon', epsilonValue);
 end
--- rbfRegression.m
+++ rbfRegression.m
@@ -1,4 +1,5 @@
 function rbfRegMdl = rbfRegression(data, targets, boxConstraint, labelColumn, epsilonValue, sigma)
+disp('rbf');
 rbfRegMdl = fitrsvm(data, targets(:, labelColumn), 'KernelFunction', 'gaussian', 'BoxConstraint', boxConstraint, ...
                  'Epsilon', epsilonValue, 'KernelScale', sigma);
 end
