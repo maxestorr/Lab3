5bf8b29e2aaa7c028b38c82c34a7c8d80a0841e8
Kan98 <kan.srimanchantha@gmail.com>
2019-12-08 20:20:25 +0000

    added display statements

--- rbfClassification.m
+++ rbfClassification.m
@@ -1,5 +1,5 @@
 function rbfClassMdl = rbfClassification(data, labels, boxConstraint, labelColumn, sigma)
-    disp('rbf')
+    disp('rbf');
     rbfClassMdl = fitcsvm(data, labels(:, labelColumn), 'KernelFunction', 'gaussian', ...
         'BoxConstraint', boxConstraint, 'KernelScale', sigma);
 end
